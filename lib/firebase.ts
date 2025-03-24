ab7231278e19b66628a8301f0e9058479c5dd956
Firmin B. <stereogaming18@gmail.com>
2025-03-24 15:26:33 +0100

    fix: Firebase creds -> .env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,20 +1,24 @@
-import { initializeApp, getApps } from 'firebase/app';
-import { getAuth, setPersistence, browserSessionPersistence } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp, getApps } from "firebase/app";
+import {
+  getAuth,
+  setPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 /**
  * Firebase configuration object containing the keys and identifiers for your Firebase project.
  * This object is required to initialize the Firebase app.
  */
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 /**
@@ -28,10 +32,9 @@ export const auth = getAuth(app);
  * Set the authentication persistence to session storage.
  * This means that the authentication state will be persisted across page reloads but not across sessions (i.e., when the user closes the browser).
  */
-setPersistence(auth, browserSessionPersistence)
-  .catch((error) => {
-    console.error("Error setting auth persistence:", error);
-  });
+setPersistence(auth, browserSessionPersistence).catch((error) => {
+  console.error("Error setting auth persistence:", error);
+});
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
