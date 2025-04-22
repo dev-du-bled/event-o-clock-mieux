3af0e760379c4ce31eb9d0715bf8e4d623db73a5
Zigleur <eden.douru@hotmail.com>
2025-04-22 22:25:29 +0200

    fix: if statement inutile

--- components/navigation/profile-menu.tsx
+++ components/navigation/profile-menu.tsx
@@ -56,72 +56,71 @@ export default function ProfileMenu() {
       </>
     );
 
-  if (user)
-    return (
-      <div className="relative items-center flex" ref={menuRef}>
-        <Link
-          href="/cinema/cart"
-          className="hover:text-primary relative flex items-center px-4 py-2 text-gray-600 transition-colors"
-        >
-          <ShoppingCart className="mr-1 h-5 w-5" />
-          Panier
-          {/* {cartItemsCount > 0 && (
+  return (
+    <div className="relative items-center flex" ref={menuRef}>
+      <Link
+        href="/cinema/cart"
+        className="hover:text-primary relative flex items-center px-4 py-2 text-gray-600 transition-colors"
+      >
+        <ShoppingCart className="mr-1 h-5 w-5" />
+        Panier
+        {/* {cartItemsCount > 0 && (
                         <span className="bg-primary absolute -top-1 -right-1 flex h-5 w-5 items-center justify-center rounded-full text-xs text-white">
                       {cartItemsCount}
                        </span>
                         )} */}
-        </Link>
-        <button
-          className="group flex text-gray-600 hover:text-primary transition-colors hover:cursor-pointer"
-          onClick={() => setIsOpen(!isOpen)}
-          data-state={isOpen ? "open" : "closed"}
-        >
-          {user.image ? (
-            <Image
-              src={user.image}
-              alt={`photo de profil de ${user.name}`}
-              className="w-6 h-6 rounded-full mr-2 object-cover"
-              height={24}
-              width={24}
-            />
-          ) : (
-            <User className="w-5 h-5 mr-1" />
-          )}
-          {user.name}
-          <ChevronDown className="transition-transform group-data-[state=open]:rotate-180" />
-        </button>
-
-        {isOpen && (
-          <div className="absolute w-50 top-10 right-0 rounded-lg shadow-lg bg-white border">
-            <div className="px-4 py-2 space-y-2">
-              <Link
-                href="/profile"
-                className="flex items-center hover:text-primary text-gray-600 transition-colors"
-              >
-                Mon Profil
-              </Link>
-              <Link
-                href="/create-event"
-                className="flex items-center hover:text-primary text-gray-600 transition-colors"
-              >
-                Créer un événement
-              </Link>
-              <Link
-                href="/my-events"
-                className="flex items-center hover:text-primary text-gray-600 transition-colors"
-              >
-                Mes événements
-              </Link>
-              <Link
-                href="/favorites"
-                className="flex items-center hover:text-primary text-gray-600 transition-colors"
-              >
-                Favoris
-              </Link>
-              <LogOutButton className="flex w-full" />
-            </div>
-          </div>
+      </Link>
+      <button
+        className="group flex text-gray-600 hover:text-primary transition-colors hover:cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+        data-state={isOpen ? "open" : "closed"}
+      >
+        {user.image ? (
+          <Image
+            src={user.image}
+            alt={`photo de profil de ${user.name}`}
+            className="w-6 h-6 rounded-full mr-2 object-cover"
+            height={24}
+            width={24}
+          />
+        ) : (
+          <User className="w-5 h-5 mr-1" />
         )}
-      </div>
-    );
+        {user.name}
+        <ChevronDown className="transition-transform group-data-[state=open]:rotate-180" />
+      </button>
+
+      {isOpen && (
+        <div className="absolute w-50 top-10 right-0 rounded-lg shadow-lg bg-white border">
+          <div className="px-4 py-2 space-y-2">
+            <Link
+              href="/profile"
+              className="flex items-center hover:text-primary text-gray-600 transition-colors"
+            >
+              Mon Profil
+            </Link>
+            <Link
+              href="/create-event"
+              className="flex items-center hover:text-primary text-gray-600 transition-colors"
+            >
+              Créer un événement
+            </Link>
+            <Link
+              href="/my-events"
+              className="flex items-center hover:text-primary text-gray-600 transition-colors"
+            >
+              Mes événements
+            </Link>
+            <Link
+              href="/favorites"
+              className="flex items-center hover:text-primary text-gray-600 transition-colors"
+            >
+              Favoris
+            </Link>
+            <LogOutButton className="flex w-full" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
